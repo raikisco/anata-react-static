479e5e13abc6041638c14dab532f161d1aa22812
tobias Gu <tobias.goo@gmail.com>
2018-03-05 10:59:15 -0800

    changed language interperetation and server side style tags

--- static.config.js
+++ static.config.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { extractCritical } from "emotion-server";
+import { renderToString } from "react-dom/server";
+
 // Though this file is optional, there are SO MANY COOL THINGS you can do here.
 // Read the docs at https://github.com/nozzle/react-static/blob/master/README.md to learn more!
 export default {
   // siteRoot: "http://www.anatamassage.com/",
   siteRoot: "http://www.tobygu.com/anata-react-static/",
   paths: {
-    src: 'src', // The source directory. Must include an index.js entry file.
-    dist: 'docs', // The production output directory.
-    devDist: 'dist', // The development scratch directory.
-    public: 'public', // The public directory (files copied to dist during build)
+    src: "src", // The source directory. Must include an index.js entry file.
+    dist: "docs", // The production output directory.
+    devDist: "dist", // The development scratch directory.
+    public: "public" // The public directory (files copied to dist during build)
   },
   Document: class CustomHtml extends React.Component {
     render() {
@@ -16,8 +19,10 @@ export default {
       // const script1 = "window.child_process = require('child_process')";
       // const script2 =
       //   "window.lazySizesConfig = window.lazySizesConfig || {}; window.lazySizesConfig.loadMode = 1"
+      let extractedComp = extractCritical(renderToString(children));
+
       return (
-        <Html lang="de">
+        <Html lang="en-us">
           <Head>
             <meta charSet="UTF-8" />
             <meta
@@ -28,12 +33,11 @@ export default {
             {renderMeta.styleTags}
             {/* <script dangerouslySetInnerHTML={{ __html: script1 }} /> */}
             {/* <script dangerouslySetInnerHTML={{ __html: script2 }} /> */}
-            {/* <script async defer src="https://apis.google.com/js/api.js" /> */}
-            <script src="https://www.paypalobjects.com/api/checkout.js" />
+            <style type="text/css">{extractedComp.css}</style>
           </Head>
           <Body>{children}</Body>
         </Html>
       );
     }
-  }
+  },
 };
